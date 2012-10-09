022cc987e6e64b476b8289099ba88aa4fc3ff600
Pascal Hartig <phartig@rdrei.net>
2012-10-09 23:39:01 +0200

    Version bump

--- twentiment/__init__.py
+++ twentiment/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'twentiment'
-__version__ = '0.0.1'
-__build__ = 0x000001
+__version__ = '0.1.0'
+__build__ = 0x001000
 __author__ = 'Pascal Hartig'
 __license__ = 'Apache 2'
 __copyright__ = 'Copyright 2012 Pascal Hartig'
